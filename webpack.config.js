79c44a455a20c5f7ce9daeec0432e01af59165a1
Arlekin1208 <arlekin12089@gmail.com>
2019-03-14 16:34:10 +0100

    update babel vue-loader packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var path = require('path')
 var webpack = require('webpack')
-
+const { VueLoaderPlugin } = require("vue-loader");
 module.exports = {
   entry: './src/main.js',
   output: {
@@ -69,6 +69,7 @@ module.exports = {
       }
     ]
   },
+   plugins: [new VueLoaderPlugin()],
   resolve: {
     alias: {
       'vue$': 'vue/dist/vue.esm.js'
